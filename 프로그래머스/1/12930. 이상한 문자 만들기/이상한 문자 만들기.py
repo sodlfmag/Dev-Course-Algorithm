76924c156844d88b45c00a1c353db6b43a6d3a47
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2023-11-21 17:04:49 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 9.93 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,11 @@
+# 문자열 s는 "한 개 이상"의 단어로 구성되어 있습니다. <- 문자열 사이의 공백이 하나가 아님에 유의
+def solution(s):
+    s = list(s)
+    cnt = 0 # variable to count word index
+    for i in range(len(s)):
+        if s[i] == ' ':
+            cnt = 0
+        else:       # odd                       even
+            s[i] = s[i].lower() if cnt % 2 else s[i].upper()
+            cnt += 1
+    return ''.join(s)
