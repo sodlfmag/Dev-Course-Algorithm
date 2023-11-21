4bef58ab7f785ad0a7ca29487227fdd334427bb8
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2023-11-21 17:22:13 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.15 ms
+메모리: 10.2 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 2일 17:20:28
+2023년 11월 2일 17:22:12
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,10 +1,10 @@
-# 문자열 s는 "한 개 이상"의 단어로 구성되어 있습니다. <- 문자열 사이의 공백이 하나가 아님에 유의
+# 문자열 s는 "한 개 이상"의 단어로 구성되어 있습니다. <- 문자열 사이의 공백이 하나가 아님에 유의!
 def solution(s):
     s = list(s)
     cnt = 0 # variable to count word index
     for i in range(len(s)):
         if s[i] == ' ':
-            cnt = 0
+            cnt = 0 # initialize cnt when meet space
         else:       # odd                       even
             s[i] = s[i].lower() if cnt % 2 else s[i].upper()
             cnt += 1
