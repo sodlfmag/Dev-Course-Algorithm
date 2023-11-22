7c00fad326a0737ef4472c673153c2d91de72b46
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2023-11-22 20:16:47 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.23 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -5,12 +5,6 @@ def solution(new_id):
     new_id = re.sub('[^-\._a-z0-9]', '', new_id)# step 2
     new_id = re.sub('\.{2,}', '.', new_id)      # step 3
     new_id = re.sub('^\.|\.$', '', new_id)      # step 4
-    if not new_id:                              # step 5
-        new_id = 'a'
-    if len(new_id) >= 16:                       # step 6
-        new_id = new_id[:15]
-        if new_id[-1] == '.':
-            new_id = new_id[:-1]
-    if len(new_id) <= 2:                        # step 7
-        new_id += new_id[-1] * (3 - len(new_id))
-    return new_id
+    new_id = 'a' if not new_id else new_id[:15] # step 5
+    new_id = re.sub('\.$', '', new_id)          # step 6
+    return new_id.ljust(3, new_id[-1])          # step 7
