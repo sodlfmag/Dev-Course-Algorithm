b5deaeb8d516d491d2cf970a226ac4b6400643cb
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2023-11-22 20:34:37 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,20 +1,11 @@
 // Use Regular Expression
 function solution(new_id) {
-    new_id = new_id.toLowerCase()       // step 1
-        .replace(/[^-\._a-z0-9]/g, '')  // step 2
-        .replace(/\.{2,}/g, '.')        // step 3
-        .replace(/^\.|\.$/g, '');       // step 4
-    if (!new_id) {                      // step 5
-        new_id = 'a';
-    }
-    if (new_id.length >= 16) {          // step 6
-        new_id = new_id.substring(0, 15);
-        if (new_id[new_id.length - 1] === '.') {
-            new_id = new_id.substring(0, new_id.length - 1);
-        }
-    }
-    if (new_id.length <= 2) {           // step 7
-        new_id += new_id[new_id.length - 1].repeat(3 - new_id.length);
-    }
-    return new_id;
+    new_id = new_id
+        .toLowerCase()                                  // step 1
+        .replace(/[^-\.\w]/g, '')                       // step 2 (\w = [a-zA-Z0-9_])
+        .replace(/\.{2,}/g, '.')                        // step 3
+        .replace(/^\.|\.$/g, '')                        // step 4
+        .replace(/^$/, 'a')                             // step 5
+        .slice(0, 15).replace(/\.$/g, '');              // step 6
+    return new_id.padEnd(3, new_id[new_id.length - 1]); // step 7
 }
