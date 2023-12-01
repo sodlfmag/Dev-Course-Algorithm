d71f55b88aadeb95efd097e0548428284a0408eb
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2023-12-01 20:39:10 +0900

    Delete 프로그래머스/1/121683. [PCCP 모의고사 #1] 1번 - 외톨이 알파벳 directory
    A commit message with '#' created an unintended link.

--- 프로그래머스/1/121683. [PCCP 모의고사 #1] 1번 - 외톨이 알파벳/[PCCP 모의고사 #1] 1번 - 외톨이 알파벳.py
+++
@@ -1,9 +0,0 @@
-# 단순 구현 O(n)
-def solution(input_string):
-    answer = {} # alphabet : is_loner(True/False)
-    prev = ''
-    for alphabet in input_string:
-        if alphabet in answer and alphabet != prev: answer[alphabet] = True # 이전 알파벳과 다른 이미 나온 알파벳
-        elif alphabet not in answer: answer[alphabet] = False   # 처음 나온 알파벳
-        prev = alphabet
-    return ''.join(sorted([k for k, v in answer.items() if v])) or 'N'
