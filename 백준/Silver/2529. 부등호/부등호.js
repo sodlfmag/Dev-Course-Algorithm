900f9621ba96696a033f32e328217e8ac34d65a6
namu56 <swi920224@gmail.com>
2023-12-08 20:19:06 +0900

    2023-12-08 알고리즘 문제풀이

--- 백준/Silver/2529. 부등호/부등호.js
+++ 백준/Silver/2529. 부등호/부등호.js
@@ -1,46 +1,46 @@
-let fs = require('fs')
-let input = fs.readFileSync('/dev/stdin').toString().split('\n')
-let k = Number(input[0])
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let k = Number(input[0]);
 
-// 부등호 넣기
-let inequality = input[1].split(' ')
+// 부등호 배열에 넣기
+let inequality = input[1].split(" ");
 
-// 0~9까지 방문 처리할 배열 만들기
-let visited = new Array(10).fill(false)
+// 0~9까지 방문 처리 배열 만들기
+let visited = new Array(10).fill(false);
 
-let result = []
-let maxValue = ""
-let minValue = ""
+// 순열, 최댓값, 최솟값 설정
+let selected = [];
+let maxValue = "";
+let minValue = "";
 
-// 부등호 만족할 때, 최댓값, 최솟값 출력
 function dfs(depth) {
-    if(depth == k + 1) {
-        let check = true // 부등호 만족하는지 확인
-        for(let i = 0; i < k; i++) {
-            if (inequality[i] == "<") {
-                if(result[i] > result[i + 1]) check = false
-            } else if(inequality[i] == ">") {
-                if(result[i] < result[i + 1]) check = false
-            }            
-        }
-        if(check) { // 모든 수가 부등호 만족한다면
-            maxValue = ""
-            for(let x of result) maxValue += x + ""
-            if(minValue == "") minValue = maxValue 
-        }
-        return
+  if (depth == k + 1) {
+    let check = true;
+    // 숫자가 부등호에 알맞게 배치되는지 확인
+    for (let i = 0; i < k; i++) {
+      if (inequality[i] == "<") {
+        if (selected[i] > selected[i + 1]) check = false;
+      } else if (inequality[i] == ">") {
+        if (selected[i] < selected[i + 1]) check = false;
+      }
     }
-    for(let i = 0; i < 10; i++) {
-        if(visited[i]) continue
-        visited[i] = true
-        result.push(i)
-        dfs(depth + 1)
-        visited[i] = false
-        result.pop()
+    // 알맞게 배치된다면
+    if (check) {
+      maxValue = "";
+      for (let x of selected) maxValue += x + "";
+      if (minValue == "") minValue = maxValue;
     }
+    return;
+  }
+  for (let i = 0; i < 10; i++) {
+    if (visited[i]) continue;
+    visited[i] = true;
+    selected.push(i);
+    dfs(depth + 1);
+    visited[i] = false;
+    selected.pop();
+  }
 }
 
-dfs(0)
-console.log(maxValue + "\n" + minValue)
-
-
+dfs(0);
+console.log(`${maxValue}\n${minValue}`);
