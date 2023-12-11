9bd93cbaf5b5aa513f92494e88582571fcfb5724
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2023-12-11 17:31:00 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.81 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -2,11 +2,16 @@
     * 행렬 곱셈 : A(m x k) x B(k x n) = AB(m x n)
     * 계산 : $\sum_{x=1}^{k} A_{ix}B_{xj} = AB_{ij}$
 """
-def solution(arr1, arr2):
-    m, k, n = len(arr1), len(arr2), len(arr2[0])
-    answer = [[0] * n for _ in range(m)]
+# 정석 풀이
+def solution(A, B):
+    m, k, n = len(A), len(B), len(B[0])
+    answer = [[0] * n for i in range(m)]
     for i in range(m):
         for j in range(n):
             for x in range(k):
-                answer[i][j] += arr1[i][x] * arr2[x][j]
+                answer[i][j] += A[i][x] * B[x][j]
     return answer
+
+# zip()과 unpacking(*)을 이용한 풀이
+def solution(A, B):
+    return [[sum(a * b for a, b in zip(A_row, B_col)) for B_col in zip(*B)] for A_row in A] # zip(*B)로 B를 unpacking 후 다시 zip으로 묶어서 열로 나타냄
