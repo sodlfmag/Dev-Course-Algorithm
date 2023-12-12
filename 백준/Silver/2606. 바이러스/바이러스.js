cbe28720d3dce8a8dfd28490a78e983b0f33676d
namu56 <swi920224@gmail.com>
2023-12-12 23:36:54 +0900

    2023-12-12 알고리즘 문제풀이

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,25 +1,25 @@
-let fs =require('fs')
-let input = fs.readFileSync('/dev/stdin').toString().split('\n')
-let n = Number(input[0])
-let m = Number(input[1]) // 6
-let graph = []
-// 컴퓨터 번호에 대한 인접리스트 생성 ex)1번 컴퓨터와 연결된 모든 컴퓨터 [2, 5]
-for(let i = 1; i <=n; i++) graph[i] = []
-for(let i = 2; i <= m + 1; i++) {
-    let [x, y] = input[i].split(' ').map(Number)
-    // ex)[x, y] 는 [1, 2], [1, 5] 이런 형태 즉, graph[1]에 y좌표만 넣는다
-    graph[x].push(y)
-    graph[y].push(x)
-}
-let cnt = 0
-let visited = Array(n + 1).fill(false)
-function dfs(x) {
-    visited[x] = true
-    cnt++
-    for(y of graph[x]) {
-        if(!visited[y]) dfs(y) 
-    }
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let n = Number(input[0]);
+let m = Number(input[1]);
+let graph = [];
+for (let i = 1; i <= n; i++) graph[i] = [];
+// 현재 노드와 인접한 노드들을 그래프로 표현
+for (let i = 2; i <= m + 1; i++) {
+  let [currentNode, nextNode] = input[i].split(" ").map(Number);
+  graph[currentNode].push(nextNode);
+  graph[nextNode].push(currentNode);
 }
 
-dfs(1)
-console.log(cnt - 1)
+let cnt = 0;
+let visited = new Array(n + 1).fill(false);
+function dfs(currentNode) {
+  visited[currentNode] = true; // 현재 노드 방문 처리
+  cnt++;
+  // 현재 노드와 연결된 다른 노드 방문
+  for (nextNode of graph[currentNode]) {
+    if (!visited[nextNode]) dfs(nextNode);
+  }
+}
+dfs(1);
+console.log(cnt - 1); // 1번 노드를 제외한 수
