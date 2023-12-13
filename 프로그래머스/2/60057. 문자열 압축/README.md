c7ca0570a42dde4d9a4aee58accd36c1d2bab3ce
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2023-12-13 22:19:05 +0900

    [level 2] Title: 문자열 압축, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 33.5 MB, 시간: 0.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 21:41:1
+2023년 12월 3일 22:19:4
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -0,0 +1,17 @@
+const solution = s => {
+  const range = [...Array(s.length)].map((_, i) => i + 1);
+  return Math.min(...range.map(i => compress(s, i).length));
+};
+
+const compress = (s, n) => {
+  const make = ([a, l, c]) => `${a}${c > 1 ? c : ''}${l}`;
+  return make(
+    chunk(s, n).reduce(
+      ([a, l, c], e) => e === l ? [a, l, c + 1] : [make([a, l, c]), e, 1],
+      ['', '', 0]
+    )
+  );
+};
+
+const chunk = (s, n) =>
+  s.length <= n ? [s] : [s.slice(0, n), ...chunk(s.slice(n), n)];
