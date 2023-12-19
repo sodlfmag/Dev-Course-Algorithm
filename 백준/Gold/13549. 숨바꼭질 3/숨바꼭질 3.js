c091a7c84cd1ea324f54de9b22cbf0d7411a7427
hongii <93701887+hongii@users.noreply.github.com>
2023-12-19 23:24:10 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 1016 ms, Memory: 18312 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -7,24 +7,23 @@ let input = fs.readFileSync(filePath).toString().split("\n");
 // n, k 추출
 const [n, k] = input[0].trim().split(" ").map(Number); // 수빈이 위치: n, 동생 위치: k
 
-/* 풀이 코드 */
-const Dijkstra = () => {
+/* sol 2) 가중치가 다른 문제이므로 다익스트라로 구현해야함. 하지만, js에서는 최소힙이나 우선순위큐를 직접 구현해야함..
+따라서, 우선순위가 가장 높은 경우 -> now*2(걸린시간 : 0초) 를 먼저 처리해준다.
+(우선순위가 높은 node부터 queue에 차곡차곡 넣어짐 -> 걸린시간이 최소가 되는 node 부터 빼낼 수 있음)*/
+const BFS2 = () => {
   const q = [];
   const time = new Array(100001).fill(Infinity);
   time[n] = 0; // 출발점에서의 시간 = 0
   q.push([0, n]); // [걸린시간, 위치]
 
   while (q.length > 0) {
-    const t = Math.min(...q.map((arr) => arr[0])); // 최소 시간 찾기 === 다익스트라의 최소힙에서 최솟값을 구하는 것
-    const nowIdx = q.map((arr) => arr[0]).indexOf(t); // 최소시간을 가진 위치 idx 찾기
-    const now = q[nowIdx][1]; // 현재 위치
-    q.splice(nowIdx, 1); // 파이썬의 최소힙에서 heappop() 역할 -> 최소 시간을 pop
+    const [t, now] = q.shift();
 
     if (t > time[now]) continue;
 
     if (now === k) return time[k];
 
-    const dt = [now * 2, now - 1, now + 1]; // 이동가능한 다음 위치 배열
+    const dt = [now * 2, now - 1, now + 1]; // 우선순위가 높은 now * 2를 가장 먼저 방문함
     dt.forEach((next) => {
       if (0 <= next && next <= 100000) {
         if (next === now * 2 && time[next] > t) {
@@ -39,4 +38,4 @@ const Dijkstra = () => {
   }
 };
 
-console.log(Dijkstra());
+console.log(BFS2());
