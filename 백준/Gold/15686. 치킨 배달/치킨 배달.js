b6b3d11e7d155f3079140f03a68d304b3205638b
namu56 <swi920224@gmail.com>
2023-12-22 16:56:17 +0900

    2023-12-22 알고리즘 문제풀이

--- 백준/Gold/15686. 치킨 배달/치킨 배달.js
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.js
@@ -1,49 +1,53 @@
-let fs = require('fs')
-let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let [n, m] = input[0].split(' ').map(Number) // 도시의 크기 N(행), 치킨 집 수(M)
+let [n, m] = input[0].split(" ").map(Number);
 
-let chicken = [] // 치킨집 위치 배열
-let house = [] // 집 위치 배열
+// 전체 도시의 좌표 중 집과 치킨집의 좌표를 따로 배열에 넣어주기
+let house = [];
+let chicken = [];
 
-// 전체 도시 정보 중 치킨집과 집의 위치를 배열에 넣어주기
-for(let i = 1; i <= n; i++) {
-    let line = input[i].split(' ').map(Number)
-    for(let j = 0; j < n; j++) {
-        if(line[j] == 1) house.push([i, j])
-        if(line[j] == 2) chicken.push([i, j])
-    }
+for (let i = 1; i <= n; i++) {
+  let line = input[i].split(" ").map(Number);
+  for (let j = 0; j < n; j++) {
+    if (line[j] == 1) house.push([i, j]);
+    if (line[j] == 2) chicken.push([i, j]);
+  }
 }
 
-let visited = new Array(chicken.length).fill(false) // 치킨집 방문 여부 확인
-let selected = [] // m개의 치킨집의 좌표 인덱스
+// 치킨 집 방문 처리 배열
+let visited = new Array(chicken.length).fill(false);
+// 선택된 치킨 집들의 좌표 인덱스
+let selected = [];
+// 정답 초기 값 설정
+let answer = 1e9;
+dfs(0, 0);
+console.log(answer);
 
-let answer = 1e9
-dfs(0, 0)
-console.log(answer)
-
+// 치킨 집들 중에서 m개를 고른 후, m개의 조합들 중 도시 치킨 거리가 가장 작은 최솟값 구하기
 function dfs(depth, start) {
-    if(depth == m) {
-        let result = [] // 선택된 m개의 치킨 집의 좌표 넣기
-        for(let i of selected) result.push(chicken[i])
-        let sum = 0 // 치킨 거리(집과 치킨 집과의 최소 거리)의 합
-        for(let [hx, hy] of house) {
-            let temp = 1e9
-            for(let [cx, cy] of result) {
-                temp = Math.min(temp, Math.abs(hx - cx) + Math.abs(hy - cy))
-            }
-            sum += temp
-        }
-        answer = Math.min(answer, sum) // 도시 치킨 거리의 최솟값 구하기
-        return
+  if (depth == m) {
+    let result = []; // 조합들의 치킨집 좌표 넣을 곳
+    for (let i of selected) result.push(chicken[i]);
+    let sum = 0;
+    // 치킨 거리들의 합(치킨 도시 거리) 중 최소값 구하기
+    for (let [hx, hy] of house) {
+      let temp = 1e9;
+      // 집에서 치킨 집까지의 거리들 중 최소 거리 구하기(치킨 거리)
+      for (let [cx, cy] of result) {
+        temp = Math.min(temp, Math.abs(hx - cx) + Math.abs(hy - cy));
+      }
+      sum += temp;
     }
-    // start 지점부터 하나씩 원소 인덱스 확인
-    for(let i = start; i < chicken.length; i++) {
-        if(visited[i]) continue
-        selected.push(i)
-        visited[i] = true
-        dfs(depth + 1, i + 1)
-        selected.pop()
-        visited[i] = false
-    }
+    answer = Math.min(answer, sum);
+    return;
+  }
+  for (let i = start; i < chicken.length; i++) {
+    if (visited[i]) continue;
+    visited[i] = true;
+    selected.push(i);
+    dfs(depth + 1, i + 1);
+    visited[i] = false;
+    selected.pop();
+  }
 }
